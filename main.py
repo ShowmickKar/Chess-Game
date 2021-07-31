d7d99f4dbeb5429eab0d9e05ae64c6c483ec9673
Showmick Kar <showmick12@gmail.com>
2021-07-31 17:14:56 +0600

    Implemented Game Finish and Restart Scene

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import pygame
 from chess_board import ChessBoard
+from pygame import mixer
+
 
 pygame.init()
 pygame.font.init()
+mixer.init()
 
 WIDTH, HEIGHT = (1000, 600)
 
@@ -15,24 +18,42 @@ def render_graphics(window, board):
     board.draw(window)
     pygame.display.update()
 
+
 def end_screen(window, board, winner):
-    pygame.draw.rect(window, (91,96,100), (601, 0, WIDTH - BOARD_SIZE, HEIGHT))
+    pygame.draw.rect(window, (35, 35, 35), (601, 0, WIDTH - BOARD_SIZE, HEIGHT))
     small_font = pygame.font.SysFont("Sans Serif", 30)
     large_font = pygame.font.SysFont("Sans Serif", 50)
-    restart_message = small_font.render("Press ANY KEY to restart", False, (255, 0, 0))
+    restart_message = small_font.render(
+        "Press ANY KEY to restart", False, (255, 255, 255)
+    )
     window.blit(restart_message, (700, 10))
     if winner == "draw":
         message = large_font.render("STALEMATE!", False, (0, 0, 0))
         window.blit(message, (700, 250))
     else:
-        winner.upper()
-        message = (
-            large_font.render("WHITE Wins!", False, (0, 0, 0))
-            if winner == "white"
-            else large_font.render("BLACK Wins!", False, (0, 0, 0))
+        winning_cause = (
+            large_font.render("CHECKMATE!", False, (255, 0, 0))
+            if board.winning_cause == "CHECKMATE"
+            else large_font.render("TIME OUT!", False, (255, 0, 0))
         )
+        message = (
+            large_font.render("WHITE Wins!", False, (255, 0, 0))
+            if winner == "white"
+            else large_font.render("BLACK Wins!", False, (255, 0, 0))
+        )
+        window.blit(winning_cause, (690, 100))
         window.blit(message, (700, 250))
     pygame.display.update()
+
+
+def restart(window):
+    window.fill((35, 35, 35))
+    large_font = pygame.font.SysFont("Sans Serif", 100)
+    message = large_font.render("Restarting...", False, (255, 0, 0))
+    window.blit(message, (320, 250))
+    pygame.display.update()
+    pygame.time.delay(3000)
+    main()
 
 
 def main():
@@ -41,6 +62,7 @@ def main():
     clock = pygame.time.Clock()
     board = ChessBoard(BOARD_SIZE)
     render_graphics(window, board)
+    counter = 0
     run = True
     while run:
         clock.tick(10)
@@ -59,9 +81,12 @@ def main():
                     """
                     Restart the Game
                     """
-                    pygame.time.delay(2000)
-                    main()
+                    restart(window)
         if board.winner != None:
+            if not counter:
+                game_finish_sound = pygame.mixer.Sound("Assets/music/Game_Finish.wav")
+                game_finish_sound.play()
+                counter = not counter
             end_screen(window, board, board.winner)
             continue
 
